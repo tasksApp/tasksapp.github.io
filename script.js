da1d70a7fd1b785a7b1843be573be1820e7a8132
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2017-10-15 19:25:01 +0200

    edited

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://intense-fjord-94464.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://intense-fjord-94464.herokuapp.com/v1/trello/';
+  const apiRoot = 'http://intense-fjord-94464.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://intense-fjord-94464.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
