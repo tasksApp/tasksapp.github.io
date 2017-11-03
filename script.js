2c2364b9fac01ad3c5e896c887d00b1f50787c74
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2017-11-03 09:05:56 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://aqueous-inlet-19968.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://aqueous-inlet-19968.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://aqueous-inlet-19968.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://aqueous-inlet-19968.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
